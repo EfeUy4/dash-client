ffc93b8dc067343d2b112014d9f7834b3d764fc9
storm-beyndtech <khameleonstorm@gmail.com>
2026-04-28 13:34:47 +0100

    storage

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,9 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_PAYSTACK_PUBLIC_KEY: string
-  readonly VITE_CLOUDINARY_CLOUD_NAME: string
-  readonly VITE_CLOUDINARY_UPLOAD_PRESET: string
-  readonly VITE_APP_NAME: string
+readonly VITE_APP_NAME: string
   readonly VITE_APP_DESCRIPTION: string
   readonly VITE_APP_URL: string
   readonly VITE_GA_TRACKING_ID: string
